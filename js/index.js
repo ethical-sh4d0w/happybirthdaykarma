d9e77559ba49da195f579c98ff50b2189a457d37
RodrigoEthical <rodrigojorge.ethical@gmail.com>
2026-07-12 23:39:53 -0400

    Primer commit

---
+++ js/index.js
@@ -0,0 +1,39 @@
+(function() {
+  function $(id) {
+    return document.getElementById(id);
+  }
+
+  var card = $('card'),
+      openB = $('open'),
+      closeB = $('close'),
+      timer = null;
+
+  function setInitialState() {
+    if (window.innerWidth <= 430) {
+      card.setAttribute('class', 'open-fully');
+    } else {
+      card.setAttribute('class', '');
+    }
+  }
+
+  setInitialState();
+
+  openB.addEventListener('click', function () {
+    card.setAttribute('class', 'open-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', 'open-fully');
+      timer = null;
+    }, 1000);
+  });
+
+  closeB.addEventListener('click', function () {
+    card.setAttribute('class', 'close-half');
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(function () {
+      card.setAttribute('class', '');
+      timer = null;
+    }, 1000);
+  });
+
+}());
